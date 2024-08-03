5cdd7bc163f1f513602cb4ab32b99dbb8cfef6f0
Dan0poco <141448993+Dan0poco@users.noreply.github.com>
2024-08-03 06:49:36 -0700

    Add files via upload

---
+++ lotto.py
@@ -0,0 +1,18 @@
+dicetopdwn = ("...")
+
+
+
+print(dicetopdwn)
+
+import random
+
+
+print(random.randint(1,90))
+print(random.randint(1,90))
+print(random.randint(1,90))
+print(random.randint(1,90))
+print(random.randint(1,90))
+
+
+print(dicetopdwn)
+print()
